d0b7aa67de14c3aada087fafcb879b3885c41218
shuma-matsuzawa <shumatsu.23@gmail.com>
2023-03-23 23:41:47 +0900

    webページ作成の学習

---
+++ src/components/BlogPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className="container">
+
+        <h1>ブログ</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur?
+        </p>
+    </div>
+  )
+}
+
+export default BlogPage
